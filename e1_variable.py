b3795f0bb8e2a67daac44d3c53cbdce70f3bb0cf
Nankpak <nankpaktanfatanko@gmail.com>
2025-08-12 11:35:16 +0100

    slicing commit

---
+++ e1_variable.py
@@ -0,0 +1,11 @@
+firstname = 'Nankpak'
+lastname = 'Tanko'
+age = 10
+are_you_married = False
+height = 1.79
+num = -1/2
+num2 = -1//2
+print(lastname[-3:])
+print(lastname[::-1])
+print(type(num),num)
+print(type(num2),num2)
---
+++ ex2_slicing.py
@@ -0,0 +1,99 @@
+# 1
+s1 = "PythonProgramming"
+print(s1[0:6])
+print(s1[6:])
+print(s1[3:10])
+print(s1[:1])
+print(s1[10:17])
+
+# 2
+s2 = "DataScience"
+print(s2[:4])
+print(s2[4:])
+print(s2[1:7])
+print(s2[2:5])
+print(s2[5:])
+
+# 3
+s3 = "MachineLearning"
+print(s3[:7])
+print(s3[7:])
+print(s3[1:8])
+print(s3[3:9])
+print(s3[5:])
+
+# 4 (with space)
+s4 = "Artificial Intelligence"
+print(s4[:10])
+print(s4[10:])
+print(s4[1:11])
+print(s4[4:])
+print(s4[5:15])
+
+# 5
+s5 = "Blockchain"
+print(s5[:5])
+print(s5[5:])
+print(s5[1:4])
+print(s5[2:])
+print(s5[3:9])
+
+# 6
+s6 = "Cryptocurrency"
+print(s6[:6])
+print(s6[6:])
+print(s6[1:5])
+print(s6[2:8])
+print(s6[4:])
+
+# 7 (with space)
+s7 = "Programming Language"
+print(s7[:11])
+print(s7[11:])
+print(s7[1:8])
+print(s7[3:])
+print(s7[5:15])
+print()
+# 8)
+s8 = "ComputerVision"
+print(s8[:8])
+print(s8[8:])
+print(s8[1:7])
+print(s8[3:])
+print(s8[4:12])
+print()
+# 9 (with space))
+s9 = "Deep Learning"
+print(s9[:4])
+print(s9[4:])
+print(s9[1:5])
+print(s9[2:])
+print(s9[3:10])
+print()
+# 10
+s10 = "NaturalLa)nguageProcessing"
+print(s10[:7])
+print(s10[7:])
+print(s10[1:9])
+print(s10[3:])
+print(s10[5:17])
+print()
+# 11
+s11 = "OpenAICha)tbot"
+print(s11[:6])
+print(s11[6:])
+print(s11[1:4])
+print(s11[2:])
+print(s11[4:12])
+print()
+# 12
+s12 = "QuantumCo)mputing"
+print(s12[:7])
+print(s12[7:])
+print(s12[1:9])
+print(s12[3:])
+print(s12[5:1])
+# 13
+s13="Augmented)Reality"
+print(s13[:9])
+print(s13[9:])
